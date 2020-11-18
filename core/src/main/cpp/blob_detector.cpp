228112acbe3231d95428e423e39e838556a5406f
serivesmejia <serivesmejia@gmail.com>
2020-11-18 10:59:49 -0700

    Initial commit

---
+++ core/src/main/cpp/blob_detector.cpp
@@ -0,0 +1,95 @@
+#include <limits>
+
+#include <jni.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/features2d.hpp>
+
+#include "blob_detector.h"
+
+using namespace cv;
+using namespace std;
+
+SimpleBlobDetector_Context::SimpleBlobDetector_Context(const Ptr<SimpleBlobDetector>& detector){
+    this->detector = detector;
+}
+
+int i_max = numeric_limits<int>::max();
+
+void vecKeyPointToMat(vector<KeyPoint>& v_kp, Mat& mat) {
+
+    int count = (int)v_kp.size();
+    mat.create(count, 1, CV_32FC(7));
+
+    for(int i=0; i<count; i++) {
+        KeyPoint kp = v_kp[i];
+        mat.at< Vec<float, 7> >(i, 0) = Vec<float, 7>(kp.pt.x, kp.pt.y, kp.size, kp.angle, kp.response, (float)kp.octave, (float)kp.class_id);
+    }
+
+}
+
+extern "C"
+JNIEXPORT jlong JNICALL
+Java_com_github_serivesmejia_nativeblobdetector_NativeBlobDetector_nativeCreateBlobDetector(JNIEnv *env, jobject thiz, jdouble minArea, jdouble minCircularity, jdouble maxCircularity) {
+
+    SimpleBlobDetector::Params params;
+
+    params.thresholdStep = 10;
+    params.minThreshold = 50;
+    params.maxThreshold = 220;
+
+    params.minRepeatability = 2;
+    params.minDistBetweenBlobs = 10;
+
+    params.filterByColor = true;
+    params.blobColor = 255;
+
+    params.filterByArea = true;
+    params.minArea = minArea;
+    params.maxArea = i_max;
+
+    params.filterByCircularity = true;
+    params.minCircularity = minCircularity;
+    params.maxCircularity = maxCircularity;
+
+    params.filterByInertia = true;
+    params.minInertiaRatio = 0.1;
+    params.maxInertiaRatio = i_max;
+
+    params.filterByConvexity = true;
+    params.minConvexity = 0.95;
+    params.maxConvexity = i_max;
+
+    auto* blobDetCtx = new SimpleBlobDetector_Context(SimpleBlobDetector::create(params));
+    return (jlong) blobDetCtx;
+
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_github_serivesmejia_nativeblobdetector_NativeBlobDetector_nativeDetectBlobs(JNIEnv *env, jobject thiz, jlong blob_det_ptr, jlong input_ptr, jlong output_ptr) {
+
+    auto* blobDetCtx = (SimpleBlobDetector_Context*) blob_det_ptr;
+
+    Mat* input = (cv::Mat*) input_ptr;
+    Mat* output = (cv::Mat*) output_ptr;
+
+    vector<KeyPoint> keyPoints = vector<KeyPoint>();
+
+    blobDetCtx->detector->detect(*input, keyPoints);
+
+    vecKeyPointToMat(keyPoints, *output);
+
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_github_serivesmejia_nativeblobdetector_NativeBlobDetector_nativeReleaseBlobDetector(JNIEnv *env, jobject thiz, jlong blob_det_ptr) {
+
+    auto* blobDetCtx = (SimpleBlobDetector_Context*) blob_det_ptr;
+
+    blobDetCtx->detector->clear();
+    delete blobDetCtx->detector;
+    delete blobDetCtx;
+
+}
---
+++ core/src/main/java/com/github/serivesmejia/nativeblobdetector/NativeBlobDetector.java
@@ -0,0 +1,43 @@
+package com.github.serivesmejia.nativeblobdetector;
+
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfKeyPoint;
+
+public class NativeBlobDetector {
+
+    static {
+        System.loadLibrary("TeamCode");
+    }
+
+    long nativeObj;
+
+    public NativeBlobDetector(double minArea, double minCircularity, double maxCircularity) {
+        nativeObj = nativeCreateBlobDetector(minArea, minCircularity, maxCircularity);
+    }
+
+    public MatOfKeyPoint detect(Mat input) {
+        MatOfKeyPoint keyPoints = new MatOfKeyPoint();
+        nativeDetectBlobs(nativeObj, input.nativeObj, keyPoints.nativeObj);
+        return keyPoints;
+    }
+
+    public void release() {
+        nativeReleaseBlobDetector(nativeObj);
+    }
+
+    @Override
+    protected void finalize() {
+        release();
+    }
+
+    public static class Parameters {
+
+    }
+
+    private static native long nativeCreateBlobDetector(double minArea, double minCircularity, double maxCircularity);
+
+    private static native void nativeDetectBlobs(long blobDetPtr, long inputPtr, long outputPtr);
+
+    private static native void nativeReleaseBlobDetector(long blobDetPtr);
+
+}
